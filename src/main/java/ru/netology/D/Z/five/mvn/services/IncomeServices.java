6cb62ee947d3578e81e06190db36cb8de873652a
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2023-03-08 14:11:37 +0300

    the solution of the problem 1. 2.

---
+++ src/main/java/ru/netology/D/Z/five/mvn/services/IncomeServices.java
@@ -0,0 +1,20 @@
+package ru.netology.D.Z.five.mvn.services;
+
+public class IncomeServices {
+    public int calculate(int income, int expense) {
+        int count = 0;
+        int threshold = 0;
+
+        for (int month = 0; month < 12; month++) {
+
+            if (threshold >= income * 2) {
+                count++;
+                threshold = threshold / 3;
+
+            } else {
+                threshold = threshold + (income - expense);
+            }
+        }
+        return count;
+    }
+}
